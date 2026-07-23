7288302e02b8fda608e1717a4bc53c26a9f47d20
Aditi Yadav <aditiyadav9124@gmail.com>
2026-07-23 11:53:56 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,12 @@
+console.log('Aditi Yadav');
+//write a funtion to take two numbers and return their sum
+function sum(a,b){
+
+}
+const add=(a,b) =>{
+    return a+b;
+};
+const add2 =(a,b) =>a+b;
+console.log(sum(20,10));
+console.log(add(20,10));
+console.log(add(20,10));
