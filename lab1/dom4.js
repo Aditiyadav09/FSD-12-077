bd110569ad18d12755b2ca5ccad2f88363e00b08
Aditi Yadav <aditiyadav9124@gmail.com>
2026-07-30 11:51:17 +0530

    lab1 completed

---
+++ lab1/dom4.js
@@ -0,0 +1,10 @@
+import { EventEmitter } from "events";
+
+const form = new EventEmitter();
+
+form.on("submit",(uname,password)=>{
+    console.log("form submitted😏😏😏");
+    console.log(`user name:${uname}`);
+    console.log(`user password ;${password}`);
+});
+form.emit("submit","Aditi Yadav","11223322");
